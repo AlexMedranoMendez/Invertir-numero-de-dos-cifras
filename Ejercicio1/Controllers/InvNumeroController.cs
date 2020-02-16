4e3991f35ce5cf5d4efb56d880de649712ca156b
Enmah4k <alexmm011@hotmail.com>
2020-02-16 12:20:43 -0400

    finalizado

---
+++ Ejercicio1/Controllers/InvNumeroController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ejercicio1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicio1.Controllers
+{
+    public class InvNumeroController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new InvertirNumero());
+        }
+
+        [HttpPost]
+        public IActionResult Index(InvertirNumero i, string inversion)
+        {
+            if (inversion == "inv")
+            {
+                i.InvNum = Invertir(i.Num).ToString("D2");
+            }
+
+            return View(i);
+        }
+
+        private int Invertir(int num)
+        {
+            int resul = 0; 
+
+            while (num > 0) 
+            { 
+                resul = resul * 10 + num % 10; num /= 10;
+            }
+
+            return resul;
+        }
+    }
+}
---
+++ Ejercicio1/Models/InvertirNumero.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ejercicio1.Models
+{
+    public class InvertirNumero
+    {
+        public int Num { get; set; }
+
+        public string InvNum { get; set; }
+    }
+}
